960695a1a3a344fea776d857212563ce1512f9aa
huzaifamk <malikhuzaifa99@gmail.com>
2023-05-24 00:40:02 +0500

    Added new application module Database_Manipulation

---
+++ Database_Manipulation/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DatabaseManipulationConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Database_Manipulation'
--- MVT_Architecture_Python/urls.py
+++ MVT_Architecture_Python/urls.py
@@ -2,9 +2,12 @@
 
 from django.contrib import admin
 from django.urls import path
-from CRUD_App import views
+from CRUD_App import views as v1
+from Database_Manipulation import views as v2
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
+    path('', v1.index, name='index'),
+    path('home/', v1.home, name='home'),
+    path('home/v2', v2.Home_v2, name='home_v2'),
 ]
