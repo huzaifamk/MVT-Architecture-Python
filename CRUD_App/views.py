21333119030a925bc4c7ee69ebb960c25f479b8a
huzaifamk <malikhuzaifa99@gmail.com>
2023-05-24 00:39:39 +0500

    Added routes

--- CRUD_App/views.py
+++ CRUD_App/views.py
@@ -3,4 +3,8 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Landing Page")
+    return HttpResponse()
+
+
+def home(request):
+    return HttpResponse("Home Page")
