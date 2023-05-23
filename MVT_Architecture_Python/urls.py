66aa7dd5f35fc1e482082b8fa67d99d3984e7720
huzaifamk <malikhuzaifa99@gmail.com>
2023-05-24 01:59:11 +0500

    Added routes and created templates

--- MVT_Architecture_Python/urls.py
+++ MVT_Architecture_Python/urls.py
@@ -9,6 +9,7 @@ from Database_Manipulation import views as v2
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', v1.index, name='index'),
+    path('create/', v1.create, name='create'),
     path('home/', v1.home, name='home'),
     path('home/v2', v2.Home_v2, name='home_v2'),
     path('dbInterface/', include('DB_Interface.urls')),
