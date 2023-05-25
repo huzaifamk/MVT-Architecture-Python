925cb1ea87f5e0cbe004a04d920ae252fdcc5bc4
huzaifamk <malikhuzaifa99@gmail.com>
2023-05-25 21:44:45 +0500

    Revamped models

--- CRUD_App/models.py
+++ CRUD_App/models.py
@@ -7,3 +7,7 @@ class User(models.Model):
     user_name = models.CharField(max_length=50)
     email = models.EmailField(max_length=50)
     password = models.CharField(max_length=50)
+
+
+def __str__(self):
+    return 'User: ' + self.first_name + ' ' + self.last_name + ' ' + self.user_name + ' ' + self.email + ' ' + self.password
